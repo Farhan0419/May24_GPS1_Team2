aa3307535a5e2e733065c887649b0d3463beba8a
Farhan0419 <farhan.khanfh2004@gmail.com>
2025-06-14 20:57:11 +0800

    Movement Done

--- Assets/Scrips/PlayerMovement.cs
+++ Assets/Scrips/PlayerMovement.cs
@@ -1,4 +1,67 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
- 
-// Movement
+using UnityEngine.Rendering;
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Transform GroundCheck;
+    public LayerMask GroundLayer;
+
+    private float horizontal;
+    public float speed = 8f;
+    public float jumpPower = 16f;
+    private bool isFacingRight = true;
+    private bool movementDisabled = false;
+
+    private void Update()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+
+        if (!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!movementDisabled)
+        {
+            if (context.performed && IsGrounded())
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            }
+            else if (context.canceled && rb.linearVelocity.y > 0f)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            }
+        }
+
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(GroundCheck.position, 0.2f, GroundLayer);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localscale = transform.localScale;
+        localscale.x *= -1f;
+        transform.localScale = localscale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (!movementDisabled)
+        {
+            horizontal = context.ReadValue<Vector2>().x;
+        }
+    }
+
+}
