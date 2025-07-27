1071c64c756ee3f8d1356f82dcb57f67fb552050
FarhansSlave <piemotor1113@gmail.com>
2025-07-27 20:30:55 +0800

    CRUSHER ART & ESC BUTTON

--- Assets/Pause.cs
+++ Assets/Pause.cs
@@ -4,26 +4,44 @@ using UnityEngine.SceneManagement;
 public class Pause : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    public static bool isPaused = false;
+
+    void pauseAction()
+    {
+        if (isPaused)
+        {
+
+            Resume();
+        }
+        else
+        {
+            PauseB();
+        }
+    }
+
 
     public void PauseB()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+        isPaused = true;
         Debug.Log("IT WORKS");
     }
 
     public void Home()
     {
-        SceneManager.LoadScene("Main Menu");
+        SceneManager.LoadScene("MainMenu");
     }
 
-    public void Resume ()
+    public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        isPaused = false;
+        Debug.Log("Game Resume");
     }
 
-    public void Restart ()
+    public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1f;
