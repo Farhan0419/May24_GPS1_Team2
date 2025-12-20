c82d1be205724ec8c991f67e311823f9d776860c
Farhan0419 <farhan.khanfh2004@gmail.com>
2025-12-20 17:00:21 +0800

    Props added + virtual mouse activation fix

--- Assets/Scrips/VirtualMouse/CursorScript.cs
+++ Assets/Scrips/VirtualMouse/CursorScript.cs
@@ -1,12 +1,18 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CursorScript : MonoBehaviour
 {
     [SerializeField] private GameObject cursor;
+    private CanvasGroup canvas;
+    [SerializeField] private bool alwaysActive;
 
     private void Start()
     {
-        ToggleCursor(false);
+        canvas = GetComponent<CanvasGroup>();
+        if (alwaysActive) ToggleCursor(true);
+        else ToggleCursor(false);
     }
     private void OnEnable()
     {
@@ -19,6 +25,7 @@ public class CursorScript : MonoBehaviour
 
     private void ToggleCursor(bool paused)
     {
+        if (alwaysActive) return;
         if (paused)
         {
             cursor.SetActive(true);
@@ -26,6 +33,31 @@ public class CursorScript : MonoBehaviour
         else
         {
             cursor.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        var lastDevice = InputSystem.devices.FirstOrDefault(d => d.lastUpdateTime == InputSystem.devices.Max(d => d.lastUpdateTime));
+
+        if (lastDevice != null)
+        {
+            if (lastDevice is Keyboard || lastDevice is Mouse)
+            {
+                canvas.alpha = 0;
+            }
+            else if (lastDevice is Gamepad)
+            {
+                canvas.alpha = 1;
+            }
+            else
+            {
+                Debug.Log("Using other device: " + lastDevice.name);
+            }
+        }
+        else
+        {
+            Debug.Log("No input device detected");
         }
     }
 }
